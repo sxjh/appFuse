45d7d1d42ca1ca8581ff02287218a74404591174
sxjh <surenj_00@yahoo.com>
2015-12-04 16:51:12 -0800

    Update GitTest2.java

--- Sample/src/GitTest2.java
+++ Sample/src/GitTest2.java
@@ -1,5 +1,6 @@
 public class GitTest2 {
 	public static void main(String args[]) {
 		System.out.println("Git is easy to use and modified in GitHub and mnve to Loacal repository!!!");
+		System.out.println("Again modified in GitHub");
 	}
 }
