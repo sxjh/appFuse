f7a9b3b9871d0d504d71d09991f95ea1185c54c9
Surendar Jannamaraju <surenj_00@yahoo.com>
2015-12-04 21:03:29 -0800

    push to GitHub.

--- Sample/src/GitTest2.java
+++ Sample/src/GitTest2.java
@@ -2,5 +2,6 @@ public class GitTest2 {
 	public static void main(String args[]) {
 		System.out.println("Git is easy to use and modified in GitHub and mnve to Loacal repository!!!");
 		System.out.println("Again modified in GitHub");
+		System.out.println("Push to GitHub!!!");
 	}
 }
